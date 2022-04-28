882eddda8ae5aa70d15d8f603b7e49746bed7347
hilaglanz <hilaglanz@gmail.com>
2022-04-28 16:14:35 +0300

    fixed plottingdir

--- make_plots.py
+++ make_plots.py
@@ -26,8 +26,9 @@ def plot_range(value='rho', snapshotDir= "output", plottingDir="plots", firstSna
     for snap in range(firstSnap,lastSnap,skipSteps):
         print("doing snapshot ",snap)
         loaded_snap = gadget_readsnap(snap, snapshotDir)
-        loaded_snap.plot_Aslice(value,logplot=logplot,colorbar=True, center= center, vrange=vrange, box=box, res=res)
-
+        print(loaded_snap.nparts)
+        pc = loaded_snap.plot_Aslice(value,logplot=logplot,colorbar=True, center= center, vrange=vrange, box=box, res=res)
+        print(pc)
         if box == False:
             box=[loaded_snap.boxsize,loaded_snap.boxsize]
         if plot_points:
