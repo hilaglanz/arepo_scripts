ce28c85e93daf5a70bbee068114229b029688f6a
hilaglanz <hilaglanz@gmail.com>
2022-04-28 15:21:44 +0300

    submit simple plotting task

--- submit_script.sh
+++ submit_script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 #SBATCH -n 1
-#SBATCH --time=3-0
 #SBATCH -J arepo_plotting
 
 ### Output files 
 #SBATCH --output=/storage/ph_hagai/glanz/ThickAcretion/plots/job.%J.out.txt
 #SBATCH --error=/storage/ph_hagai/glanz/ThickAcretion/plots/job.%J.err.txt
+#SBATCH --partition=ph_hagai
+#SBATCH --time=3-0
 
-export SOURCE_DIR = "/storage/ph_hagai/glanz/ThickAcretion/output"
-export SAVING_DIR = "/storage/ph_hagai/glanz/ThickAcretion/plots"
+export SOURCE_DIR="/storage/ph_hagai/glanz/ThickAcretion/output"
+export SAVING_DIR="/storage/ph_hagai/glanz/ThickAcretion/plots"
 
-export arepoPy = "/usr/local/ph_hagai/anaconda3/envs/amuse-env/bin/python-3.10"
+export arepoPy="/usr/local/ph_hagai/anaconda3/envs/amuse-env/bin/python-3.10"
 
 $arepoPy make_plots.py --source_dir=$SOURCE_DIR --saving_dir=$SAVING_DIR
 
