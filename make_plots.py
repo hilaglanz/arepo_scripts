95c6199b692edd29fb181f278004aec2f841eb1e
hilaglanz <hilaglanz@gmail.com>
2022-04-28 15:04:51 +0300

    added arg parser

--- make_plots.py
+++ make_plots.py
@@ -4,9 +4,9 @@ import numpy as np
 from loadmodules import *
 
 
-def plot_range(value='rho',outputDir="output", firstSnap=0,lastSnap=-1,skipSteps=1,box=False,vrange=False,logplot=True, res=1024,
+def plot_range(value='rho', snapshotDir= "output", plottingDir="plots", firstSnap=0,lastSnap=-1,skipSteps=1,box=False,vrange=False,logplot=True, res=1024,
                center=True,plot_points=True, additional_points_size=30,additional_points_shape='X', additional_points_color='w', units_length = 'cm'):
-    snapshots = glob.glob('./snapshot_*')
+    snapshots = glob.glob(snapshotDir + './snapshot_*')
     maxSnap=len(snapshots)
     if lastSnap == -1:
         lastSnap = maxSnap
@@ -15,6 +15,9 @@ def plot_range(value='rho',outputDir="output", firstSnap=0,lastSnap=-1,skipSteps
     if firstSnap > lastSnap:
         print("cannot do firstSnap > lastSnap")
         return
+
+    if not os.path.exists(plottingDir):
+        os.mkdir(plottingDir)
 
     for snap in range(firstSnap,lastSnap + 1,skipSteps):
         loaded_snap = gadget_readsnap(snap,outputDir)
@@ -40,10 +43,49 @@ def plot_range(value='rho',outputDir="output", firstSnap=0,lastSnap=-1,skipSteps
         np.savefig(outputDir + 'Aslice_' + value + '_' + str(snap) + '.jpg')
         print("saved fig")
 
+def InitParser():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--beginStep', type=int,  help='first step', default=0)
+    parser.add_argument('--lastStep', type=int,  help='last step', default=-1)
+    parser.add_argument('--skipStep', type=int, help='number of steps to skip', default=1)
+    parser.add_argument('--source_dir', type=str,  help='path to snapshot files directory', default= sys.argv[0])
+    parser.add_argument('--saving_dir', type=str,  help='path to output directory', default= "plots")
+    parser.add_argument('--value', type=str,  help='value to be plotted', default= "rho")
+    parser.add_argument('--vmin', type=float,  help='minimal range plotting', default=None)
+    parser.add_argument('--vmax', type=float,  help='maximum range plotting', default=None)
+    parser.add_argument('--boxsize', type=float,  help='boxsize', default=None)
+    parser.add_argument('--logplot', type=lambda x: (str(x).lower() in ['true', '1', 'yes']),  help='logplot',
+                        default=True)
+    parser.add_argument('--res', type=int, help='plotting resolution', default=1024)
+    parser.add_argument('--center_x', type=float, help='point on x axis to be the center of the plot', default=None)
+    parser.add_argument('--center_y', type=float, help='point on y axis to be the center of the plot', default=None)
+    parser.add_argument('--plot_points', type=lambda x: (str(x).lower() in ['true', '1', 'yes']),  help='logplot',
+                        default=True)
+    parser.add_argument('--additional_points_size', type=float,  help='point sizes in plot', default = 30)
+    parser.add_argument('--additional_points_shape', type=str,  help='point shapes in plot', default= "X")
+    parser.add_argument('--additional_points_color', type=str,  help='point colors in plot', default= "w")
 
+    return parser
 
 def main(args):
-    return
+    parser = InitParser()
+    args = parser.parse_args()
+    box = False
+    if args.boxsize is not None:
+        box = [args.boxsize, args.boxsize]
+
+    vrange = False
+    if args.vmin is not None and args.vmax is not None:
+        vrange = [args.vmin, args.vmax]
+
+    center = False
+    if args.center_x is not None and args.center_y is not None:
+        center = [args.center_x, args.center_y]
+
+    plot_range(args.value, args.source_dir, args.saving_dir, args.beginStep, args.lastStep, args.skipSteps, box=box,
+               vrange=vrange,logplot=args.logplot, res=args.res, center=center, plot_points=args.plot_points,
+               additional_points_size=args.additional_points_size, additional_points_shape=args.additional_points_shape,
+               additional_points_color=args.additional_points_color)
 
 if __name__ == "__main__":
     for arg in sys.argv:
