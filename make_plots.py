90fdb2b560975a8cdfd693e3f64111f8586ed9a4
hilaglanz <hilaglanz@gmail.com>
2022-10-23 17:50:10 +0300

    modify units only once

--- make_plots.py
+++ make_plots.py
@@ -59,6 +59,7 @@ def plot_single_value(loaded_snap, value='rho',box=False, vrange=False,logplot=T
         loaded_snap.data["pos"][:,0] *= name_and_units["length"][2]
         loaded_snap.data["pos"][:,1] *= name_and_units["length"][2]
         loaded_snap.data["pos"][:,2] *= name_and_units["length"][2]
+        loaded_snap.center *= name_and_units["length"][2]
         '''loaded_snap.vel[:,0] *= name_and_units["vel"][2]
         loaded_snap.vel[:,1] *= name_and_units["vel"][2]
         loaded_snap.vel[:,2] *= name_and_units["vel"][2]'''
