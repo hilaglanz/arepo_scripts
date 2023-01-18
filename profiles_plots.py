149ed6fe4c9b9d4883b45360eaaa3cff4752f8b4
hilaglanz <hilaglanz@gmail.com>
2023-01-18 17:38:14 +0200

    option to plot line profile along the axis of motion

--- profiles_plots.py
+++ profiles_plots.py
@@ -160,8 +160,8 @@ def get_line_profile_for_snapshot(around_density_peak, around_objects, center, m
         cell_indices = np.where(
             (s.pos[:,(motion_axis + 1) % 3] - center[(motion_axis + 1) % 3][None] < 2 * s.data["vol"] ** (1.0 / 3)) &
             (s.pos[:,(motion_axis + 2) % 3] - center[(motion_axis + 2) % 3][None] < 2 * s.data["vol"] ** (1.0 / 3)))
-    p = np.column_stack(s.data[testing_value][cell_indices],
-                        s.data["pos"][cell_indices, motion_axis] - center[motion_axis][None])
+    p = np.column_stack((s.data[testing_value][cell_indices],
+                        s.data["pos"][cell_indices, motion_axis] - center[motion_axis][None]))
 
     return p, s, suffix, testing_value
 
