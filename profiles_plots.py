c376b457179a8a513bffd3cede0f05429084dc07
Hila Glanz <hilaglanz@gmail.com>
2023-01-20 13:17:45 +0200

    row stack instead of column stack

--- profiles_plots.py
+++ profiles_plots.py
@@ -161,7 +161,7 @@ def get_line_profile_for_snapshot(around_density_peak, around_objects, center, m
             ((s.pos[:,(motion_axis + 1) % 3] - center[(motion_axis + 1) % 3] < 2 * s.data["vol"] ** (1.0 / 3))) &
             ((s.pos[:,(motion_axis + 2) % 3] - center[(motion_axis + 2) % 3] < 2 * s.data["vol"] ** (1.0 / 3))))
     print(s.get_radprof(testing_value, center).shape)
-    p = np.column_stack((s.data[testing_value][cell_indices],
+    p = np.row_stack((s.data[testing_value][cell_indices],
                          (s.data["pos"][cell_indices, motion_axis] - center[motion_axis]))[1])
     print(p.shape)
 
