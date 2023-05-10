a9a4dbe62d2525ef15848cf3f60a1e742d174c8e
Hila Glanz <hilaglanz@gmail.com>
2023-05-10 15:43:53 -0700

    conversion of acce units

--- make_plots.py
+++ make_plots.py
@@ -95,6 +95,7 @@ def plot_single_value(loaded_snap, value='rho', cmap="hot", box=False, vrange=Fa
     convert_to_cgs = True
     if unit_velocity is not None:
         basic_units["vel"].unit = unit_velocity
+        basic_units["acce"].unit = r'$' + unit_velocity + '^2 /' + unit_length + '$'
         basic_units["time"].unit = r'$' + unit_length + "/" + unit_velocity + '$'
         convert_to_cgs = False
 
