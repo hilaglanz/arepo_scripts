d49cf48623f9bb080995dfebcc4cb0a346f53f44
hilaglanz <hilaglanz@gmail.com>
2023-08-06 12:09:32 +0300

    option to plot evolution of same value at each figure

--- make_plots.py
+++ make_plots.py
@@ -403,7 +403,7 @@ def plot_single_value_evolutions(value=['rho'], snapshotDir= "output", plottingD
 
     for index, val in enumerate(value):
         print(val)
-        fig = figure(figsize=(36, 20))
+        fig = figure(figsize=(36, 17))
         if horizontal:
             fig.subplots_adjust(wspace=0, hspace=0.35)
         else:
@@ -437,6 +437,7 @@ def plot_single_value_evolutions(value=['rho'], snapshotDir= "output", plottingD
                                   axes=get_single_value(axes_array, index), ignore_types=ignore_types, colorbar=(snap_i+1==num_figures),
                               plot_xlabel=((not horizontal) and (snap_i == num_figures)),
                               plot_ylabel=((horizontal) and (snap_i == 0)))
+            subplot(curr_subplot)
             suptitle('time : {:.2g}'.format(loaded_snap.time) + " [" + basic_units["time"].unit + "]", fontsize='x-large')
             rcParams.update({'font.size': 40, 'font.family': 'Serif'})
             rcParams['text.usetex'] = True
