3758d1a09a4ac57ec92a62f72c33abb6b0c78896
Hila Glanz <hilaglanz@gmail.com>
2023-10-05 16:21:58 +0200

    option to replace the inner binary with a single point mass

--- CircumbinaryTCE.py
+++ CircumbinaryTCE.py
@@ -6,8 +6,7 @@ from loadmodules import *
 from stellar_ics.tools import *
 from stellar_ics.multiple_star import MultipleSystem, SnapshotComponent, PointMassComponent
 from BinariesICs import *
-
-
+from plot_multiples import get_obj_index
 def initialized_new_data(npart, npart0):
     data = {}
     data['pos'] = np.zeros((npart, 3))
@@ -32,6 +31,36 @@ def copy_old_data(snapshot):
     data['boxsize'] = snapshot.boxsize
 
     return data
+
+def ReplaceInnerBinaryWithPointMass(snapshot_file, new_file_name, obj1_id, obj2_id):
+    snapshot = gadget_readsnapname(snapshot_file)
+    obj1_ind = get_obj_index(snapshot, obj1_id)
+    obj2_ind = get_obj_index(snapshot, obj2_id)
+    new_pos = ((snapshot.pos[obj1_ind] * snapshot.mass[obj1_ind]).sum() +
+               (snapshot.pos[obj2_ind] * snapshot.mass[obj2_ind]).sum()) / \
+              (snapshot.mass[obj1_ind] + snapshot.mass[obj2_ind])
+
+    new_vel = ((snapshot.vel[obj1_ind] * snapshot.mass[obj1_ind]).sum() +
+               (snapshot.vel[obj2_ind] * snapshot.mass[obj2_ind]).sum()) / \
+              (snapshot.mass[obj1_ind] + snapshot.mass[obj2_ind])
+
+    new_data = initialized_new_data(snapshot.npart - 1, snapshot.npart - 3)
+    new_data['boxsize'] = snapshot.boxsize
+
+    for value in snapshot.data:
+        if value in ["boxsize"]:
+            continue
+        new_data[value][:-1] = np.delete(snapshot.data[value], [obj1_ind, obj2_id])
+
+    new_data['pos'][-1] = new_pos
+    new_data['vel'][-1] = new_vel
+    new_data['mass'][-1] = snapshot.mass[obj1_ind] + snapshot.mass[obj2_ind]
+    new_data['type'][-1] = snapshot.type[obj1_ind]
+
+    gadget_write_ics(new_file_name, new_data, format='hdf5', double='double')
+
+
+
 
 def AddPointMassToFile(snapshot_file, new_file_name, point_mass, separation, initial_inner_separation=None,
                        velocity=None):
@@ -74,6 +103,8 @@ def InitParser():
     parser.add_argument('--outer_separation', type=float, help='initial separation between the binary objects in Rsun',
                         default=1000)
     parser.add_argument('--ic_file_name', type=str, help='path to save the ic file', default="tce.ic.dat")
+    parser.add_argument('--replace_id1', type=int, help='id of the first obj to remove and replace', default=None)
+    parser.add_argument('--replace_id2', type=int, help='id of the second obj to remove and replace', default=None)
     return parser
 
 
@@ -83,6 +114,13 @@ if __name__ == "__main__":
     print(len(sys.argv))
     parser = InitParser()
     args = parser.parse_args()
+
+    if args.replace_id1 is not None and args.replace_id2 is not None:
+        ReplaceInnerBinaryWithPointMass(snapshot_file=args.giant_snapshot_file, new_file_name=args.ic_file_name,
+                                        obj1_id=args.replace_id1, obj2_id=args.replace_id2)
+        print("replaced 2 point masses with 1")
+        return
+
     AddPointMassToFile(args.giant_snapshot_file, new_file_name=args.ic_file_name,
                        separation=args.outer_separation, point_mass=args.outer_mass * msol,
                        initial_inner_separation=args.initial_inner_separation)
