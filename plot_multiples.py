0624af21263998039f7ff0f84971e2c93352559d
Hila Glanz <hilaglanz@gmail.com>
2023-10-06 15:36:00 +0200

    can have multiple _

--- plot_multiples.py
+++ plot_multiples.py
@@ -141,7 +141,12 @@ def plot_value_range(snapshot_list, snapshot_dir, plotting_dir, value, core_id=1
             values.append(get_velocity(snapshot, tertiary_id, snapshot.pos[get_obj_index(snapshot, core_id)], core_id,
                                          take_inner_mass=take_inner_mass))
         elif "surrounding" in value:
-            values.append(get_surrounding_value(snapshot, around_object_id, surrounding_radius, value.split('_')[-1]))
+            testing_value_splitted = value.split('_')
+            if len(testing_value_splitted) > 2:
+                testing_value = "_".join(testing_value_splitted[1:])
+            else:
+                testing_value = testing_value_splitted[-2]
+            values.append(get_surrounding_value(snapshot, around_object_id, surrounding_radius, testing_value))
 
         elif value == "drag":
             values.append(get_drag(snapshot, around_object_id, center=snapshot.pos[get_obj_index(snapshot, core_id)],
