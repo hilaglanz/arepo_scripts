c670f04105640d05f727121024f8d99f06057846
hilaglanz <hilaglanz@gmail.com>
2023-12-27 17:47:11 +0200

    facecolor

--- plot_saved_heatmap.py
+++ plot_saved_heatmap.py
@@ -27,7 +27,7 @@ def plot_from_pickle(heatmap_pickle,stream_pickle, sink_pickle, vmin, vmax, cmap
 
     streamplot(stream.pos_x, stream.pos_y, stream.vel_x, stream.vel_y, density=2, color='black')
 
-    circ = Circle((sink.pos_x, sink.pos_y), sink.radius, fill=True, facecolor='white', linewidth=2.0,
+    circ = Circle((sink.pos_x, sink.pos_y), sink.radius, fill=True, facecolor='white', linewidth=1.0,
                   linestyle="solid", edgecolor="black")
     gca().add_patch(circ)
 
