fe8164e457e41d3c205aadd57c3f29acd1d80bb8
hilaglanz <hilaglanz@gmail.com>
2024-02-08 16:05:20 +0200

    using resolution decrement instead of angle changes

--- create_sinks_ic.py
+++ create_sinks_ic.py
@@ -105,7 +105,7 @@ def create_ic_with_sink(ic_path, boxsize=32, G=6.672*10**-8, mach=1.4, cs=1, rho
     if not binary:
         #background = initialize_dictionary_with_point_masses(rho,1,Rs*0.1)
         maximum_cell_radius, sphere_size, pos = create_a_radial_gowing_mesh(Rs, minimum(100 * Rs, boxsize),
-                                                                            Rs / surroundings)
+                                                                            Rs / surroundings, resolution=surroundings)
         data={}
         data["pos"] = pos
         data["boxsize"] = sphere_size
