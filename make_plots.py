913b9d02c87dc0f7236c084bea5bf1e00fc0b8e7
hila glanz <hilaglanz@gmail.com>
2026-05-08 16:06:25 -0700

    make heatmap plots relative to center / central_id

--- make_plots.py
+++ make_plots.py
@@ -359,7 +359,7 @@ def get_value_at_inf(value, data):
 
 def calculate_label_and_value(loaded_snap, value, relative_to_sink_id, central_id=None, center=None,
                               species_file=r"..\species55.txt"):
-    if center == None:
+    if center is None:
         center = loaded_snap.center()
 
     if "xnuc" in value:
